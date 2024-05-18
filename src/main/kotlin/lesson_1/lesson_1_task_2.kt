ce8bf022653a79cc4e1b4eb1584517e3db08e162
AndreySabatDevelop <sabatarow260@gmail.com>
2024-05-18 08:38:18 +0300

    KS-1-2 do smth

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders = 75
+    val congratulationsText = "Спасибо за покупку товара, приходите ещё!"
+    println(numberOfOrders)
+    println(congratulationsText)
+    // val numberOfEmployees = 2000
+    // println(numberOfEmployees)
+    val numberOfEmloyees = 1999
+    println(numberOfEmloyees)
+}
