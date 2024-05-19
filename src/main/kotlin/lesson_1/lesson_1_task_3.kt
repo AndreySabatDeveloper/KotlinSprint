9c00c926d2ab5f80fc6e4694cdee46a19f1e5b4e
AndreySabatDevelop <sabatarow260@gmail.com>
2024-05-19 18:16:34 +0300

    KS-1-3 fixed do smth

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -2,11 +2,11 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
-    println("Год полёта-" + year)
-    println("Время взлета-" + '0' + hour + ':' + '0' + minute)
-    hour = 10
-    minute = 55
-    println("Время посадки-" + hour + ':' + minute)
+    var hour = "09"
+    var minute = "07"
+    println("Год полёта - $year")
+    println("Время взлета - $hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("Время посадки - $hour:$minute")
 }
