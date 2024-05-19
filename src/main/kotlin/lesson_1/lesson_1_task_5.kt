2720b58e43e7b0371753754a9b6370d5b53e1a81
AndreySabatDevelop <sabatarow260@gmail.com>
2024-05-19 19:44:41 +0300

    KS-1-5 fixed do smth

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,19 +1,29 @@
 package org.example.lesson_1
 
 fun main() {
-    val seconds = 6_360 // время пребывания Гагариным в космосе 106 минут
+    val timeSpentInSpace = TIME_SPENT_IN_SPACE // время пребывания Гагариным в космосе 106 минут
 
-    val remainingHours = seconds/3600 // секунды поделил на колво секунд в 1 часе "3600",
+    val remainingHours = String.format(
+        "%02d",
+        timeSpentInSpace / NUMBER_OF_SECONDS_PER_HOUR
+    ) // секунды поделил на колво секунд в 1 часе "3600",
     // получилось 1,766667. Т.к Тип инт целый , вывлеось только 1 час
 
-    val remainingMinutes = seconds/60 - 60 // секунды поделил на колво секунд в 1 минуте "60",
+    val remainingMinutes =
+        String.format("%02d", timeSpentInSpace / NUMBER_OF_SECONDS_PER_MINUTE - NUMBER_OF_MINUTE_PER_HOUR)
+    // секунды поделил на колво секунд в 1 минуте "60",
     // получилось 106 минут, т.к мы печатаем в формате [часы:минуты:секунды],
     // вычил 60 минут чтобы показать остаток минут от 106 минут
 
-    val remainingSeconds = seconds - 6_360 // Гагарин пробыл в космосе ровно 106 минут
+    val remainingSeconds =
+        String.format("%02d", timeSpentInSpace - TIME_SPENT_IN_SPACE) // Гагарин пробыл в космосе ровно 106 минут
     // (по информации википедии) поэтому остаток секунд из 6_480 секунд (ровно 106 минут) нет
 
-    println("0$remainingHours:$remainingMinutes:0$remainingSeconds") // добавил "0" в часы и секунды
-// т.к число выводилось только с 1 чмслом (1:48:0)
+    println("$remainingHours:$remainingMinutes:$remainingSeconds")
 
 }
+
+const val TIME_SPENT_IN_SPACE = 6_360
+const val NUMBER_OF_SECONDS_PER_HOUR = 3_600
+const val NUMBER_OF_SECONDS_PER_MINUTE = 60
+const val NUMBER_OF_MINUTE_PER_HOUR = 60
