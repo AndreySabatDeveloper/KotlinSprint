645abddfa358511a4e11aa3248362aef7c4c421c
AndreySabatDevelop <sabatarow260@gmail.com>
2024-05-19 20:07:14 +0300

    KS-2-1 do smth

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val assessmentOfTheFirstStudent = 3.0
+    val assessmentOfTheSecondStudent = 4.0
+    val assessmentOfTheThirdStudent = 3.0
+    val assessmentOfTheFourthStudent = 5.0
+    val averageRating =
+        (assessmentOfTheFirstStudent + assessmentOfTheSecondStudent + assessmentOfTheThirdStudent + assessmentOfTheFourthStudent) / 4
+    println(averageRating)
+}
+
